48a66f89352b02bde656c29b9358848fc9c945b2
= <=>
2015-04-04 23:09:20 +0300

    Firs commit for screenshot app

--- Screenshot/ViewController.swift
+++ Screenshot/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func actionScreen(sender: AnyObject) {
+        
+        
+        takeScreenAndReturnImage()
+        
+        UIImageWriteToSavedPhotosAlbum(takeScreenAndReturnImage(), nil, nil, nil)
+        
+        
+    }
+    @IBOutlet weak var label: UILabel!
+    var timer:NSTimer?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = nil
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("timerUpdate"), userInfo: nil, repeats: true)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func takeScreenAndReturnImage()-> UIImage{
+    
+    let v = view
+        
+    UIGraphicsBeginImageContextWithOptions(v.bounds.size, true, 1.0)
+    v.drawViewHierarchyInRect(v.bounds, afterScreenUpdates: true)
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+        return img
+    }
+    
+    
+    func timerUpdate(){
+        
+        let date = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        label.text = dateFormatter.stringFromDate(date)
+    
     }
 
     override func didReceiveMemoryWarning() {

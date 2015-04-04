faad59eb143c6b2dd98b00fefd4a66f160a87a4e
= <=>
2015-04-04 18:27:24 +0300

    Initial Commit

---
+++ Screenshot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Screenshot
+//
+//  Created by Yusuf UZAN on 04/04/15.
+//  Copyright (c) 2015 hx0a1q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
